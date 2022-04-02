c390dc4016bf8637405dca4a25d90a271a53de45
VladyslavShaposhnikov <shapik2404@gmail.com>
2022-04-02 05:48:59 +0200

    admin page

--- mc_tasklist/admin.py
+++ mc_tasklist/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Feedback, Subject
+from .models import Feedback, Subject, Contact
 
 admin.site.register(Feedback)
 admin.site.register(Subject)
+admin.site.register(Contact)
